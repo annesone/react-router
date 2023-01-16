7178d87b4d4a7f853a6c81e5b71d01a9db15bd92
Anne Son <annesontech@gmail.com>
2023-01-16 01:19:24 -0800

    react-router tutorial

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+  return (
+    <section className="section">
+      <h1>Home</h1>
+    </section>
+  );
+};
+export default Home;
